aa7807d66a639ae20c86299a9b4e39c900cf4383
Argo79 <melloni6@gmail.com>
2021-12-17 13:07:49 +0100

    Add files via upload

--- scrapehttp.sh
+++ scrapehttp.sh
@@ -9,6 +9,11 @@ rm nome.txt
 echo $nomefile"scrape.txt"
 mkdir $nomefile"scrape"
 cd $nomefile"scrape"
-wget -k $1 -O "index.html"
-sed -n 's/http/\n&/gp' index.html | sed -n 's/\"/\n/p' | grep "http" > $nomefile"scrape.txt"
-wget -c -k -r -i $nomefile"scrape.txt"
+if [ -f index.html ]
+then
+echo "Il file esiste e lo cancello" 
+rm index.html
+fi
+wget  $1 -O "index.html"
+sed -n 's/https/\n&/gp' index.html | sed -n 's/\"/\n/p' | grep "https" > $nomefile"scrape.txt"
+wget -c -k -r -w 0.5 -i $nomefile"scrape.txt"
